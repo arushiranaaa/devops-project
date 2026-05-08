67f1035095ebdfd0306ce8262c0a43b65e8c993c
Arushi Rana <138686469+arushiranaaa@users.noreply.github.com>
2026-05-08 11:00:48 +0530

    Create HelloDevOps class with main and add methods

---
+++ HelloDevOps.java
@@ -0,0 +1,9 @@
+public class HelloDevOps {
+    public static void main(String[] args) {
+        System.out.println("DevOps Pipeline Active!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
